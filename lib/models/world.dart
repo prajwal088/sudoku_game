11e2cc3a81e60f6c27aec0de554b59b21e1832cd
prajwal088 <59723736+prajwal088@users.noreply.github.com>
2026-03-28 11:52:20 +0530

    Add World model class supporting batch organization and progression

---
+++ lib/models/world.dart
@@ -0,0 +1,31 @@
+class World {
+    String name;
+    List<Level> levels;
+    int currentLevelIndex;
+
+    World(this.name, this.levels) {
+        currentLevelIndex = 0;
+    }
+
+    Level get currentLevel {
+        return levels[currentLevelIndex];
+    }
+
+    void advanceToNextLevel() {
+        if (currentLevelIndex < levels.length - 1) {
+            currentLevelIndex++;
+        }
+    }
+
+    bool isCompleted() {
+        return currentLevelIndex == levels.length - 1;
+    }
+}
+
+class Level {
+    String title;
+    String description;
+    int difficulty;
+
+    Level(this.title, this.description, this.difficulty);
+}
