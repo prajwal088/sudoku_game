a049749bc04e39fafa7844204ce76b4ec9467ebe
prajwal088 <59723736+prajwal088@users.noreply.github.com>
2026-03-28 12:04:14 +0530

    Create PaginationWidget

---
+++ lib/widgets/pagination_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class PaginationWidget extends StatefulWidget {
+  final int totalPages;
+  final int currentPage;
+  final Function(int) onPageChanged;
+
+  PaginationWidget({
+    required this.totalPages,
+    required this.currentPage,
+    required this.onPageChanged,
+  });
+
+  @override
+  _PaginationWidgetState createState() => _PaginationWidgetState();
+}
+
+class _PaginationWidgetState extends State<PaginationWidget> {
+  void _goToPage(int page) {
+    if (page < 1 || page > widget.totalPages) return;
+    widget.onPageChanged(page);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(widget.totalPages, (index) {
+        final isActive = widget.currentPage == index + 1;
+        return GestureDetector(
+          onTap: () => _goToPage(index + 1),
+          child: Container(
+            margin: const EdgeInsets.all(4.0),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            decoration: BoxDecoration(
+              color: isActive ? Colors.blue : Colors.grey,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              '${index + 1}',
+              style: TextStyle(
+                color: isActive ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        );
+      })
+    );
+  }
+}
